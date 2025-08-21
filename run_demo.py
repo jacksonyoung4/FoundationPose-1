0cc14ecbf9286b8aaf15a2022316497bd37c67ad
jackson_young_4 <jky33@icloud.com>
2025-08-21 19:30:11 +0800

    Remove cv2_imshow

--- run_demo.py
+++ run_demo.py
@@ -69,7 +69,7 @@ if __name__=='__main__':
       center_pose = pose@np.linalg.inv(to_origin)
       vis = draw_posed_3d_box(reader.K, img=color, ob_in_cam=center_pose, bbox=bbox)
       vis = draw_xyz_axis(color, ob_in_cam=center_pose, scale=0.1, K=reader.K, thickness=3, transparency=0, is_input_rgb=True)
-      cv2.imshow('1', vis[...,::-1])
+      # cv2.imshow('1', vis[...,::-1]) - No GUI to visualise in runpod terminal
       cv2.waitKey(1)
 
 
